8c1741d1342b90b86a24e64e3e1e232bd488a7e1
Amarnath Rajasekaran <amarnath-r@HCL.COM>
2020-01-29 16:32:45 +0530

    assertion check

--- script2.test.js
+++ script2.test.js
@@ -1,18 +1,18 @@
 const fetch = require('node-fetch');
 const swapi = require('./script2');
 
+//swapi.getPeople(fetch).then(data => console.log(data));
+
 it('calls swapi to get people', done => {
+  expect.assertions(1);
   swapi.getPeople(fetch).then(data => {
     expect(data.count).to.equal(87);
     done();
   });
 });
 
-it('calls swapi to get people with a promise', done => {
-  expect.assertions(1);
-
+it('calls swapi to get people with a promise', () => {
   swapi.getPeoplePromise(fetch).then(data => {
     expect(data.count).to.equal(87);
-    done();
   });
 });
