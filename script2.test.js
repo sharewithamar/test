b3ef9ba10fd356c4d57b472b15730683a9dad36f
Amarnath <sharewithamar@gmail.com>
2020-01-29 22:40:05 +0530

    added asyn timeouts for external api added jest mock and spy

--- script2.test.js
+++ script2.test.js
@@ -1,26 +1,45 @@
 const fetch = require('node-fetch');
 const swapi = require('./script2');
 
-it('calls swapi to get people', done => {
+it('calls swapi to get people', async done => {
   expect.assertions(1);
 
   swapi.getPeople(fetch).then(data => {
     expect(data.count).toEqual(87);
     done();
   });
-});
+}, 10000);
 
-it('promise style- calls swapi to get people', () => {
+it('promise style- calls swapi to get people', async () => {
   expect.assertions(1);
 
   return swapi.getPeople(fetch).then(data => {
     expect(data.count).toEqual(87);
   });
-});
+}, 10000);
 
-it('calls swapi to get people with a promise', () => {
+it('calls swapi to get people with a promise', async () => {
   expect.assertions(2);
   return swapi.getPeoplePromise(fetch).then(data => {
+    expect(data.count).toEqual(87);
+    expect(data.results.length).toBeGreaterThan(5);
+  });
+}, 20000);
+
+it('getPeople returns count and results', () => {
+  const mockFetch = jest.fn().mockReturnValue(
+    Promise.resolve({
+      json: () =>
+        Promise.resolve({
+          count: 87,
+          results: [0, 1, 2, 3, 4, 5]
+        })
+    })
+  );
+  expect.assertions(4);
+  return swapi.getPeoplePromise(mockFetch).then(data => {
+    expect(mockFetch.mock.calls.length).toBe(1);
+    expect(mockFetch).toBeCalledWith('https://swapi.co/api/people');
     expect(data.count).toEqual(87);
     expect(data.results.length).toBeGreaterThan(5);
   });
